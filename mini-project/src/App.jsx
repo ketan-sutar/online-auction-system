6c7267991afd67758d253469e17edaab901f585c
ketan-sutar <sutarketan35@gmail.com>
2024-07-12 19:34:11 +0530

    first commit

---
+++ mini-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='bg-green-500'>Test tailwind</h1>
+    </>
+  )
+}
+
+export default App
